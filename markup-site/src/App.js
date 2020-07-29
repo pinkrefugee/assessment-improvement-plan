8427f5135fb19f7cc93f50edf710aa26e4c4961a
Aliaksei Palevikou <Aliaksei_Palevikou@epam.com>
2020-07-29 12:17:15 +0300

    fix flexbox markup

--- markup-site/src/App.js
+++ markup-site/src/App.js
@@ -74,10 +74,10 @@ function App() {
           <h3>Etiam veroeros lorem</h3>
           <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
           <div className='footer-icons'>
-            <i class="fa fa-twitter fa-3x" aria-hidden="true"></i>
-            <i class="fa fa-facebook fa-3x" aria-hidden="true"></i>
-            <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
-            <i class="fa fa-envelope fa-3x" aria-hidden="true"></i>
+            <a href=''><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+            <a href=''><i class="fa fa-facebook fa-3x" aria-hidden="true"></i></a>
+            <a href=''><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+            <a href=''><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a>
           </div>
         </section>
       </footer>
