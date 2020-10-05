7352477a1b73be2c952d47f3a84e567e73c142e1
Aliaksei Palevikou <Aliaksei_Palevikou@epam.com>
2020-10-05 10:27:17 +0300

    add async task

--- markup-site/src/App.js
+++ markup-site/src/App.js
@@ -1,92 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import pic01 from './assets/pic01.jpg'
-import pic02 from './assets/pic02.jpg'
-import pic03 from './assets/pic03.jpg'
-import pic04 from './assets/pic04.jpg'
-import pic05 from './assets/pic05.jpg'
-import pic06 from './assets/pic06.jpg'
 import './App.css';
 
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 function App() {
-  document.addEventListener('blur', (event) => {
-    console.log(event.target.parentElement.dataset.id);
-    event.target.parentElement.dataset.id = event.target.value;
-    event.target.value = '';
-  }, true);
+  const [posts, setPosts] = useState([]);
+  const [idInput, setIdInput] = useState('');
 
-  function showArticleId(event) {
-    const article = event.target.closest('article'); 
+  function loadData() {
+    fetch(POSTS_URL)
+    .then(response => response.json())
+    .then(json => {
+      setPosts(json.slice(0, 6));
+    })
+    .catch(error => {
+      alert(`Seems like there is some sort of a ${error}`); 
+    });
+  }
 
-    if (!article) return;
+  async function deleteData(id) {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const postsArray = posts.filter(post => post.id !== +id);
+      if (postsArray.length === posts.length) {
+        throw new Error('Post not found');
+      }
+      setPosts(postsArray);
+    } catch(err) {
+      alert(err);
+    }
+  }
 
-    alert(article.dataset.id ? article.dataset.id : 'Please set the id');
-  };
   return (
     <div className="App">
       <header>
         <section className='header-content'>
           <h1>Full Motion</h1>
           <p>Some meaningful and thoughtful text that will encourage you to do the things you don't have to</p>
-          <i class="fa fa-check-circle fa-3x" aria-hidden="true"></i>
+          <i className="fa fa-check-circle fa-3x" aria-hidden="true"></i>
         </section>
       </header>
       <main>
-        <section className='main-content' onDoubleClick={showArticleId}>
-          <article>
-            <img src={pic01} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
+        <section className='article-management'>
+          <div>
+            <button className='right-margin' onClick={loadData}>Load posts</button>
+            <button onClick={() => deleteData(idInput)}>Remove post</button>
+          </div>
+          <div>
+            <label className='right-margin'>Select article id to remove:</label>
+            <input onInput={e => setIdInput(e.target.value)}></input>
+          </div>
+        </section>
+        <section className='main-content'>
+          <>
+          {posts.map(post => {
+            return (
+            <article key={post.id}>
+              <img src={pic01} alt=''></img>
+              <div className='article-content'>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+                <button>Click</button>
+              </div>
           </article>
-          <article>
-            <img src={pic02} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
-          </article>
-          <article>
-            <img src={pic03} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
-          </article>
-          <article>
-            <img src={pic04} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
-          </article>
-          <article>
-            <img src={pic05} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
-          </article>
-          <article>
-            <img src={pic06} alt=''></img>
-            <div>
-              <h3>Where does it come from?</h3>
-              <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-              <button>Click</button>
-            </div>
-            <input></input>
-          </article>
+            )
+          })}
+          </>
         </section>
        </main>
       <footer>
@@ -94,10 +74,10 @@ function App() {
           <h3>Etiam veroeros lorem</h3>
           <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
           <div className='footer-icons'>
-            <a href=''><i class="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
-            <a href=''><i class="fa fa-facebook fa-3x" aria-hidden="true"></i></a>
-            <a href=''><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
-            <a href=''><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a>
+            <a href="/#"><i className="fa fa-twitter fa-3x" aria-hidden="true"></i></a>
+            <a href="/#"><i className="fa fa-facebook fa-3x" aria-hidden="true"></i></a>
+            <a href="/#"><i className="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+            <a href="/#"><i className="fa fa-envelope fa-3x" aria-hidden="true"></i></a>
           </div>
         </section>
       </footer>
